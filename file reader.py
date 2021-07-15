84bd7d09d32181f08c968943143c5cf145b5e85d
maynesmj <87430560+maynesmj@users.noreply.github.com>
2021-07-15 10:35:28 -0400

    newest version of code

--- file reader.py
+++ file reader.py
@@ -6,6 +6,11 @@ Created on Thu Jul  1 11:59:43 2021
 """
 
 import numpy as np
+import configparser
+config = configparser.ConfigParser()
+
+config.read('Input_Values_MC_HMS_SINGLE.ini')
+
 """
 struct_hms = open('struct_hms.inc.text')
 struct_shms = open('struct_shms.inc.text')
@@ -53,21 +58,23 @@ else:
 
 
 "Name of the file you wish to analize"
-data = "shms-20,17-26cm-monte_quads_p18_forward.txt" #this is your file
+data = "shms-2017-26cm-monte_quads_p18_forward.txt" #this is your file
 
 
 "Process of checking if the matrices are clean and if not then it will clean them"
+"""
 my_file = open(data, "r")
 string_list = my_file.readlines()
 l = len(string_list)
 
 new_strings = []
 new_new_strings = []
-
+"""
 
 """
 The actual loop that checks if the matrix numbers run into eachother
 The loop will add space between numbers of the matrix if it come true
+"""
 """
 if "0-" or "1-" or "2-" or "3-" or "4-" or "5-" or "6-" or "7-" or "8-" or "9-" in string_list:
     for string in string_list:
@@ -77,11 +84,11 @@ if "0-" or "1-" or "2-" or "3-" or "4-" or "5-" or "6-" or "7-" or "8-" or "9-" 
     new_file_contents = "".join(new_strings)
     my_file.write(new_file_contents)
     my_file.close()
-
+"""
 "Initilizing the increment variable and array names for matrix"
 n = 0
 b = 0
-
+m = 1
 
 "This finds how many matrixes are in the file"
 with open(data, "r") as f:
@@ -117,7 +124,13 @@ with open(data, "r") as d:
         if "-------------" in line:
             n += 1  
         elif line.startswith('!N'):
-            names.append(line[7:])
+            x = line[7:]
+            if 'DIPOLE_MID' in x:
+                new_x = x + '_{0}'.format(m)
+                names.append(new_x)
+                m += 1
+            else:
+                names.append(x)
         elif line.startswith('!R'):
             regions.append(line[9:])
         elif line.startswith('!O'):
@@ -157,10 +170,7 @@ for i in range(0,b):
         print("length :", lengths[i])
         print(Matrix[i])
 
-mul = np.dot(Matrix[0], Matrix[1])
-print("Matrix 0 and Matrix 1 multiplied together")
-print("")
-print(mul)
+
 
 """
 m2 = me2
@@ -176,3 +186,48 @@ elif p_flag == 4:
     m2 = mk2
 """
 
+"""
+set variables based on config input file
+"""
+print("")
+print("Variable in config file")
+for key in config['DEFAULT']:
+    print(key)
+print("")  
+print("value for how many monte carlo interations") 
+monte_carlo_interations = config['DEFAULT']['monte-carlo trials']
+spectrometer = config['DEFAULT']['spectrometer (1=hms, 2=shms, 3=..)']
+spectrometer_momentum = config['DEFAULT']['sectrometer momentum (in mev/c)']
+spectrometer_angle = config['DEFAULT']['spectrometer angle (in degrees)']
+mc_dp_p_down_limit = config['DEFAULT']['m.c. dp/p  down limit']
+mc_dp_p_up_limit = config['DEFAULT']['m.c. dp/p  up   limit']
+mc_theta_down_limit_mr = config['DEFAULT']['m.c. theta down limit (mr)']
+mc_theta_down_limit_mr_2 = config['DEFAULT']['m.c. theta down limit (mr)_2']
+mc_phi_down_limit_mr = config['DEFAULT']['m.c. phi   down limit (mr)']
+mc_phi_down_limit_mr_2 = config['DEFAULT']['m.c. phi   down limit (mr)_2']
+h_beam_spot_size = config['DEFAULT']['horiz beam spot size in cm (full width of +/- 1 sigma)']
+v_beam_spot_size = config['DEFAULT']['vert  beam spot size in cm (full width of +/- 1 sigma)']
+target_thickness = config['DEFAULT']['thickness of target (full width, cm)']
+raster_full_width_x = config['DEFAULT']['raster full-width x (cm)']
+raster_full_width_y = config['DEFAULT']['raster full-width y (cm)']
+dp_p_reconstruction_cut = config['DEFAULT']['dp/p  reconstruction cut (half width in % )']
+theta_reconstruction_cut = config['DEFAULT']['theta reconstruction cut (half width in mr)']
+phi_reconstruction_cut = config['DEFAULT']['phi   reconstruction cut (half width in mr)']
+ztgt_reconstruction_cut = config['DEFAULT']['ztgt  reconstruction cut (half width in cm)']
+one_radiation_length_target_material = config['DEFAULT']['one radiation length of target material (in cm)']
+beam_x_offset = config['DEFAULT']['beam x offset (cm)  (+x = beam left)']
+beam_y_offset = config['DEFAULT']['beam y offset (cm)  (+y = up)']
+target_z_offset = config['DEFAULT']['target z offset (cm) (+z = downstream (0.25))']
+spectrometer_x_offset = config['DEFAULT']['spectrometer x offset (cm) (+x = down)']
+spectrometer_y_offset = config['DEFAULT']['spectrometer y offset (cm)']
+spectrometer_z_offset = config['DEFAULT']['spectrometer z offset (cm)']
+spectrometer_xp_offset = config['DEFAULT']['spectrometer xp offset (mr)']
+spectrometer_yp_offset = config['DEFAULT']['spectrometer yp offset (mr)']
+particle_id = config['DEFAULT']['particle identification :(e=0, p=1, d=2, pi=3, ka=4)']
+flag_mul_scat = config['DEFAULT']['flag for multiple scattering']
+flag_wire_chamber_smearing = config['DEFAULT']['flag for wire chamber smearing']
+flag_storing_all_events = config['DEFAULT']['flag for storing all events (including failed events with stop_id > 0)']
+flag_beam_energy = config['DEFAULT']['flag for beam energy, if >0 then calculate for c elastic']
+flag_use_sieve = config['DEFAULT']['flag to use sieve']
+
+print(monte_carlo_interations)
